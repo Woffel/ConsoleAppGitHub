9011b99df6245fde7051afd0d68d25fae6a8be02
Woffel <hannes.lindros@gmail.com>
2022-09-12 14:07:38 +0200

    class Test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("New Branch");
+
+Test testing = new Test();
+
+class Test
+{
+    public Test()
+    {
+        Console.WriteLine("testing");
+    }
+    public void test()
+    {
+        Console.WriteLine("tset");
+    }
+}
